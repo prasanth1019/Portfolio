57e883d12564ec0d376e046099c60025e2878e16
praman357 <prasanth_ramanathan@comcast.com>
2022-05-31 11:44:37 +0530

    email validation

--- src/Contact.js
+++ src/Contact.js
@@ -1,21 +1,29 @@
 import React from "react";
 
+import EmailValidation from "./Validation";
+
 const Contact = () => {
+
     const[name, setName] = React.useState('Prasanth R');
+    const[notValidEmail, setNotValidEmail] = React.useState(false);
     
     const handleSubmit = (event) => {
-
-        const name = event.target[0].value;
-        const emailID = event.target[1].value;
-        const message = event.target[2].value;
-
-        window.location = "mailto:prasanthram1019@ymail.com?subject=Message from " + name + "&body= My message is " + message +"%0D%0A%0D%0A%0D%0A Regards, %0D%0A" + name + ", %0D%0A " + emailID
+            const emailID = document.getElementById('email').value;
+        if (EmailValidation(emailID)) {
+            setNotValidEmail(false);
+            const name = document.getElementById('name').value;
+            const message = document.getElementById('message').value;
+            window.location = "mailto:prasanthram1019@ymail.com?subject=Message from " + name + "&body= My message is " + message +"%0D%0A%0D%0A%0D%0A Regards, %0D%0A" + name + ", %0D%0A " + emailID;
+        } else {
+            setNotValidEmail(true);
+            return false;
+        }
     }
 
     return (
         <>
         <section id="contactus" className="" >
-            <form onSubmit={ e => handleSubmit(e) } id="contact-us-form" >
+            <form id="contact-us-form" >
                 <table width="100%">
                     <thead> Feel free to contact me! </thead>
                     <tbody>
@@ -24,7 +32,7 @@ const Contact = () => {
                                     <p id="name-span-container" >
                                         <label>Your name</label>
                                     </p>
-                                    <input name='name' type='text' ></input>
+                                    <input name='name' type='text' id="name" ></input>
                                 </td>
                         </tr>
                         
@@ -33,21 +41,25 @@ const Contact = () => {
                                 <p id="email-span-container">
                                     <label>Your email address</label>
                                 </p>
-                                    <input name='email' type='text' ></input>
+                                    <input name='email' type='text' id="email" ></input>
+                                    <p className={`errorMsg ${notValidEmail ? 'showError' : 'hideError'}`} id="email-span-container" >
+                                        Please enter a valid email address
+                                    </p>
+                                    {/* className={`errorMsg round-lg ${this.state.valid ? '' : 'error'}`} */}
                             </td>
                         </tr>
 
                         <tr>
                             <td> 
                                 <p id="usermessage-span-container">
-                                    <label>Your email address</label>
+                                    <label>Your message</label>
                                 </p>
-                                    <input name='email' type='text' ></input>
+                                    <input name='message' type='text' id="message" ></input>
                             </td>
                         </tr>
 
                         <tr>
-                            <input type="submit" name="contactSubmit" value="Submit" id="submit-btn" />
+                            <input type="button" name="contactSubmit" value="Submit" id="submit-btn" onClick={ e => handleSubmit(e) }/>
                         </tr>
 
                     </tbody>
---
+++ src/Validation.js
@@ -0,0 +1,5 @@
+
+export default function EmailValidation(emailAddress) {
+    let regEmail = /^[a-z0-9][\-_\.\+\!\#\$\%\&\'\*\/\=\?\^\`\{\|]{0,1}([a-z0-9][\-_\.\+\!\#\$\%\&\'\*\/\=\?\^\`\{\|]{0,1})*[a-z0-9]@[a-z0-9][-\.]{0,1}([a-z][-\.]{0,1})*[a-z0-9]\.[a-z0-9]{1,}([\.\-]{0,1}[a-z]){0,}[a-z0-9]{0,}/
+    return regEmail.test(emailAddress);
+}
